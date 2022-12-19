f2c1be986870ab5fed3792188899ebd760bc65f8
Aigul Aiupova <aigul.sadreeva@gmail.com>
2022-12-19 23:32:05 +0300

    Решение example 004

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("Введите число 1 для сравнения: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2 для сравнения: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-// int max = number2 * number2;
+
 if (number1 > number2)
 {
     Console.WriteLine($"Число {number1} больше числа {number2}");
