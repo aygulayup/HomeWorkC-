e06d1866b04c2d97796bbf5a98c9898359043550
Aigul Aiupova <aigul.sadreeva@gmail.com>
2022-12-19 23:57:24 +0300

    Решение example 006

--- Example006/Program.cs
+++ Example006/Program.cs
@@ -4,3 +4,14 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число для проверки: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} четное.");
+}
+else
+{
+    Console.WriteLine($"Число {number} нечетное.");
+}
