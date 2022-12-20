b4c5be512084b54e9778fac7cbbff3425f61fa59
Aigul Aiupova <aigul.sadreeva@gmail.com>
2022-12-20 11:08:59 +0300

    Решение example 008

--- Example008/Program.cs
+++ Example008/Program.cs
@@ -3,3 +3,13 @@
 // а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+
+}
