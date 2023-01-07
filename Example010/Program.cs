3d887e35a3473cc0ddf92b5ea275c0a87a307459
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-07 16:28:11 +0300

    Решение задачи 10

--- Example010/Program.cs
+++ Example010/Program.cs
@@ -4,3 +4,17 @@
 // 456-> 5
 // 782 -> 8
 // 918 -> 1
+
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int MakeDigit(int number)
+{
+int result = (number/10) % 10;
+return result;
+}
+
+int newDigit = MakeDigit(num);
+Console.WriteLine($"{num} -> {newDigit}");
