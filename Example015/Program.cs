edf1afca6f6dc831c3e90a79d8b9c37fd96995f5
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-07 22:42:32 +0300

    Решение задачи 15

--- Example015/Program.cs
+++ Example015/Program.cs
@@ -4,3 +4,20 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите целое число от 1 до 7.");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1 || num > 7) Console.Write("Введите корректное число: ");
+else
+{
+    if (num >= 1 && num <= 7)
+    {
+        Console.WriteLine($"{num} -> нет.");
+    } 
+    
+    else
+    {
+     Console.WriteLine($"{num} -> да.");
+    }
+    
+}
