d154cf507911bb62687d06ac7f2b2e531e7d6cb5
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-09 23:46:27 +0300

    Решение задачи 019

--- Example019/Program.cs
+++ Example019/Program.cs
@@ -6,3 +6,43 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num > 100000 || num < 10000)
+{
+    Console.WriteLine("Введите корректное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+int firstDigit = num / 10000;
+int secDigit = num / 1000 % 10;
+int fourthDigit = num / 10 % 10;
+int fifthDigit = num % 10;
+
+if (firstDigit == fifthDigit && secDigit == fourthDigit)
+{
+    Console.WriteLine($"{num} -> да");
+}
+else
+{
+    Console.WriteLine($"{num} -> нет");
+}
+
+
+
+
+
+
+
+
+
+// int MakeDigit(int number)
+// {
+// int result = (number/100)*10 + number % 10;
+// return result;
+// }
+
+// int newDigit = MakeDigit(rnd);
+// Console.WriteLine($" {rnd} -> {newDigit}");
