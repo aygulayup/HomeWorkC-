a2ad456761208a88f7488e592865fd261de3a318
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-10 00:08:26 +0300

    Исправление ошибки в 013

--- Example013/Program.cs
+++ Example013/Program.cs
@@ -7,14 +7,18 @@
 
 Console.WriteLine("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num < 99) Console.WriteLine($"{num} -> третьей цифры нет");
+
+if (num < 99 && num > -99) Console.WriteLine($"{num} -> третьей цифры нет");
 else
 {
-    while (num > 999)
+    while (num > 999 || num < -999)
 {
     num = num / 10;
 }
 int result = num % 10;
 
-Console.WriteLine(result);
+if (result < 0) Console.WriteLine(-result);
+else Console.WriteLine(result);
+
 }
+
