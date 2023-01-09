890b7fefc01b8a10e37ee725e77149dfba1d6230
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-09 23:05:53 +0300

    Решение задачи 021

--- Example021/Program.cs
+++ Example021/Program.cs
@@ -3,3 +3,30 @@
 // находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1 = InsertPositon("Введите координату Х первой точки:"); 
+int y1 = InsertPositon("Введите координату У первой точки:");
+int z1 = InsertPositon("Введите координату Z первой точки:");
+int x2 = InsertPositon("Введите координату X второй точки:"); 
+int y2 = InsertPositon("Введите координату Y второй точки:");
+int z2 = InsertPositon("Введите координату Z первой точки:");
+
+
+double distance = Distance(x1, y1, z1, x2, y2, z2);
+double disToZero = Math.Round(distance, 2, MidpointRounding.ToZero);
+
+Console.WriteLine(disToZero);
+
+
+int InsertPositon(string text)
+{
+    Console.Write(text);
+    int pos = Convert.ToInt32(Console. ReadLine());
+    return pos;
+}
+
+double Distance(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    int distance = (xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za);
+    return Math.Sqrt(distance);
+}
