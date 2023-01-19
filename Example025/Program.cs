3e5f9f278af0961b3e81c028029d71241744b56c
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-20 00:11:54 +0300

    Решение нерабочее 025

--- Example025/Program.cs
+++ Example025/Program.cs
@@ -4,3 +4,31 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int InsertDigit(string text) //Метод пользовательского ввода
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int PowerNum(int num1, int num2)
+{
+    int result = 1;
+
+    // while (num2 <= 0)
+    // {
+    //     num2 = InsertDigit("Введите корректное число B: ");
+    // }
+
+    for (int i = 1; i <= num2; i++)
+    {
+        result = num1 * num1;
+    }
+    return result;
+}
+
+int numA = InsertDigit("Введите число A: ");
+int numB = InsertDigit("Введите число В: ");
+int powerOfNum = PowerNum(numA, numB);
+
+Console.WriteLine($"Число {numA} в степени {numB} = {powerOfNum}");
