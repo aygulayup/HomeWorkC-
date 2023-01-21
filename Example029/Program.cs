640f26738bfea31ea24649c2e4a0d4f194dfb7b9
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-21 19:42:03 +0300

    Решение 029

--- Example029/Program.cs
+++ Example029/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов и 
+// выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random(); 
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100); 
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    // Console.Write($"[{arr[i]}]" + ", ");
+    string printArray = string.Join(",", $"[{arr}]");
+    printArray = Convert.ToInt32();
+    Console.Write(printArray);
+    }
+}
+
+int[] arr = CreateArray(8);
+PrintArray(arr);
+
+
