c6eb5fef2c714bfdcd0de78633881535ad197426
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-22 23:08:28 +0300

    Решение задачи 034

---
+++ Example034/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: 
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// 2. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max) //создание и заполнение массива
+{
+    int [] array = new int[size]; // новое место памяти для массива
+    Random rnd = new Random(); // новое место памяти для элемента класса Random
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)// вывод массива в консоль
+{
+    Console.Write("["); //вывод массива на экран в [скобках]
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write(array[i] + ", "); // вывод эл-ов массива через запятую
+        else Console.Write(array[i]); // обратиться к последнему элементу массива array[^1]
+    }
+    Console.Write("]");
+}
+
+int CountPositiveNum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) result++;
+    }
+    return result;
+}
+
+int sizeArray = 4, arrayMin = 100, arrayMax = 1000;
+int[] arr = CreateArrayRndInt(sizeArray, arrayMin, arrayMax);
+PrintArray(arr);
+int contPositive = CountPositiveNum(arr);
+Console.WriteLine($" -> {contPositive}");
