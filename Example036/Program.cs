35ba81a48b3917783f601c0764f74caf25116102
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-22 23:23:09 +0300

    Решение задачи 036. Не до конца

---
+++ Example036/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: 
+// 1. Задайте одномерный массив, заполненный случайными числами. 
+// 2. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max) //создание и заполнение массива
+{
+    int [] array = new int[size]; // новое место памяти для массива
+    Random rnd = new Random(); // новое место памяти для элемента класса Random
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)// вывод массива в консоль
+{
+    Console.Write("["); //вывод массива на экран в [скобках]
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write(array[i] + ", "); // вывод эл-ов массива через запятую
+        else Console.Write(array[i]); // обратиться к последнему элементу массива array[^1]
+    }
+    Console.Write("]");
+}
+
+int SumAddPositionNumb(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[ (i + 1) % 2 == 0]) result++;
+    }
+    return result;
+}
+
+
+int sizeArray = 4, arrayMin = -100, arrayMax = 100;
+int[] arr = CreateArrayRndInt(sizeArray, arrayMin, arrayMax);
+PrintArray(arr);
+int sumAddPosition = SumAddPositionNumb(arr);
+Console.WriteLine($" -> {sumAddPosition}");
