40eaff57c9f9c3a37474427732a2b158bf80a837
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-02-09 23:49:15 +0300

    Решение задачи 058

--- Example058/Program.cs
+++ Example058/Program.cs
@@ -37,30 +37,40 @@ void PrintMatrix(int [,] matrix)// вывод массива в консоль
     }
 }
 
-int [,] NewMatrix(int[,] matr1, int[,] matr2)
+
+int [,] MultipleMatrix(int[,] matr1, int[,] matr2)
 {
-    int [,] matrix = new int[matr1.GetLength(0), matr1.GetLength(1)];
+    int [,] matrix = new int[matr1.GetLength(0), matr2.GetLength(1)];
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = matr1[i, j] * matr2[i, j];
+            for (int temp = 0; temp < matrix.GetLength(1); temp++)
+            {
+                matrix[i, j] += matr1[i, temp] * matr2[temp, j];
+            }
         }
     }
     return matrix;
 }
 
 
-int[,] matrix1 = CreateMatrixRndInt(3, 4, 0, 10);
+int[,] matrix1 = CreateMatrixRndInt(2, 4, 0, 10);
 PrintMatrix(matrix1);
 Console.WriteLine();
 
-int[,] matrix2 = CreateMatrixRndInt(3, 4, 0, 10);
+int[,] matrix2 = CreateMatrixRndInt(3, 2, 0, 10);
 PrintMatrix(matrix2);
 Console.WriteLine();
 
-Console.WriteLine("Итоговая матрица: ");
-int[,] newMatrix = NewMatrix(matrix1, matrix2);
-PrintMatrix(newMatrix);
+if(matrix1.GetLength(1) != matrix2.GetLength(0)) Console.WriteLine("Умножение невозможно");
+else
+{
+    Console.WriteLine("Итоговая матрица: ");
+    int[,] multMatrix = MultipleMatrix(matrix1, matrix2);
+    PrintMatrix(multMatrix);
+}
+
+
 
 
